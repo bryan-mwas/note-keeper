345b28463d6376e14a995e3dea1ff9cecd142361
Mwathy <bryan@work.com>
2019-06-03 14:31:23 +0300

    Initial commit

---
+++ app/src/main/java/brian/mwathi/notekeeper/NoteListActivity.java
@@ -0,0 +1,57 @@
+package brian.mwathi.notekeeper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class NoteListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        initializeDisplayContent();
+    }
+
+    private void initializeDisplayContent() {
+        // Get the list reference
+        ListView listNotes = findViewById(R.id.list_notes);
+        // Get list of notes from the singleton class
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        // Populate the list view using an adapter.
+        ArrayAdapter<NoteInfo> notesAdapter = new
+                ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
+        listNotes.setAdapter(notesAdapter);
+
+        listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(NoteListActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
